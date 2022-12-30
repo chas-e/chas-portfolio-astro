4b8aa9df71a23a891d9da783ab45e820c762f058
chas-e <charles@chasengineering.dev>
2022-12-29 19:39:33 -0500

    🚧 Update config for development

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,11 @@ import node from '@astrojs/node'
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
+    // adapter: node({
+    //     mode: 'middleware',
+    // }),
     srcDir: './src',
     publicDir: './public',
     site: 'https://chasengineering.dev',
     integrations: [svelte()],
-    adapter: node({
-        // mode: 'middleware',
-    }),
 })
